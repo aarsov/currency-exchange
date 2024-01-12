3c831743736c29b68cb120a4459bd2d9efa56d20
Andrej Arsov <andrejarsovfax@gmail.com>
2024-01-12 20:10:14 +0100

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CurrencyConverterComponent } from './currency-converter/currency-converter.component';
-import { FormsModule } from '@angular/forms';
-
-import { HttpClientModule } from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { EditCurrencyComponent } from './edit-currency/edit-currency.component';
+import { HttpClientModule } from '@angular/common/http';
+import {MatSelectModule} from "@angular/material/select";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {BrowserAnimationsModule, NoopAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
     AppComponent,
     CurrencyConverterComponent,
-    EditCurrencyComponent,
-
+    EditCurrencyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule, 
+    AppRoutingModule,
+    HttpClientModule,
+    MatFormFieldModule,
+    MatSelectModule,
     FormsModule,
-    HttpClientModule
+    ReactiveFormsModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    // NoopAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
